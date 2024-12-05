9f522d0c6cce2dcadccc0c528d633fd62478dd4e
Palaolitico <p@seismotechnologies.com>
2024-12-05 19:24:13 +0100

    mvp: add matrix transpose to ppy

--- propreye-plugin/src/main/java/org/seismotech/propreye/PreprocessingToolbox.java
+++ propreye-plugin/src/main/java/org/seismotech/propreye/PreprocessingToolbox.java
@@ -1,5 +1,6 @@
 package org.seismotech.propreye;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
@@ -23,11 +24,50 @@ public class PreprocessingToolbox {
       : Character.toUpperCase(t.charAt(0)) + t.substring(1);
   }
 
+  public List<Object> concat(List<Object> xs, List<Object> ys) {
+    final ArrayList<Object> zs = new ArrayList<>(xs.size() + ys.size());
+    zs.addAll(xs);
+    zs.addAll(ys);
+    return zs;
+  }
+
+  public List<Object> repeat(List<Object> xs, int n) {
+    final ArrayList<Object> ys = new ArrayList<>(Math.max(0,n) * xs.size());
+    for (int i = 0; i < n; i++) ys.addAll(xs);
+    return ys;
+  }
+
+  public List<List<Object>> transpose(List<List<Object>> rows) {
+    return transpose(rows, null);
+  }
+
+  public List<List<Object>> transpose(List<List<Object>> rows, Object absent) {
+    final List<List<Object>> ts = new ArrayList<>();
+    for (int j = 0;; j++) {
+      final List<Object> col = new ArrayList<>();
+      boolean some = false;
+      for (final List<Object> row: rows) {
+        final Object x;
+        if (j < row.size()) {x = row.get(j); some = true;}
+        else x = absent;
+        col.add(x);
+      }
+      if (!some) break;
+      ts.add(col);
+    }
+    return ts;
+  }
+
   @SafeVarargs
   public final List<Map<String,Object>> table(List<String> colnames,
       List<Object>... rows) {
+    return table(colnames, Arrays.asList(rows));
+  }
+
+  public List<Map<String,Object>> table(List<String> colnames,
+      List<List<Object>> rows) {
     final int cols = colnames.size();
-    final List<Map<String,Object>> table = new ArrayList<>(rows.length);
+    final List<Map<String,Object>> table = new ArrayList<>(rows.size());
     for (final List<Object> row: rows) {
       if (row.size() != cols) throw new IllegalArgumentException(
         "Table width mismatch: row has with " + row.size()
@@ -39,12 +79,5 @@ public class PreprocessingToolbox {
       table.add(record);
     }
     return table;
-  }
-
-  public List<Object> concat(List<Object> xs, List<Object> ys) {
-    final ArrayList<Object> zs = new ArrayList<>(xs.size() + ys.size());
-    zs.addAll(xs);
-    zs.addAll(ys);
-    return zs;
   }
 }
